afe498928c89a420078cdcc3e22b089105cade88
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2022-03-23 16:55:27 +0200

    Student index component

--- src/app/student/index/index.component.ts
+++ src/app/student/index/index.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from '../student.service';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
+  students: Student[] = [];
 
-  constructor() { }
+  /*------------------------------------------
+  --------------------------------------------
+  Created constructor
+  --------------------------------------------
+  --------------------------------------------*/
+  constructor(public studentService: StudentService) {}
 
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
   ngOnInit(): void {
+    this.studentService.getAll().subscribe((data: Student[]) => {
+      this.students = data;
+      console.log(this.students);
+    });
   }
 
+  /**
+   * Write code on Method
+   *
+   * @return response()
+   */
+  deleteStudent(id: number) {
+    this.studentService.delete(id).subscribe((res) => {
+      this.students = this.students.filter((item) => item.id !== id);
+      console.log('Student deleted successfully!');
+    });
+  }
 }

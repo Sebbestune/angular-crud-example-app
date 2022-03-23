4845920ab03ffe747e49879b266d5f7f8d1f041c
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2022-03-23 16:38:59 +0200

    post service styling

--- src/app/post/post.service.ts
+++ src/app/post/post.service.ts
@@ -58,7 +58,11 @@ export class PostService {
       .pipe(catchError(this.errorHandler));
   }
 
-  errorHandler(error: { error: { message: string; }; status: any; message: any; }) {
+  errorHandler(error: {
+    error: { message: string };
+    status: any;
+    message: any;
+  }) {
     let errorMessage = '';
     if (error.error instanceof ErrorEvent) {
       errorMessage = error.error.message;

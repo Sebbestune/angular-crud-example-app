4bedd858002487c5044f44f6232b19a770a94fdb
sebastian.tb.lindgren <sebastian.tb.lindgren@gmail.com>
2022-03-23 15:00:43 +0200

    style edit component

--- src/app/post/edit/edit.component.ts
+++ src/app/post/edit/edit.component.ts
@@ -2,19 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Post } from '../post';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
-      
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
-       
   id!: number;
   post!: Post;
   form!: FormGroup;
-     
+
   /*------------------------------------------
   --------------------------------------------
   Created constructor
@@ -24,8 +23,8 @@ export class EditComponent implements OnInit {
     public postService: PostService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
-     
+  ) {}
+
   /**
    * Write code on Method
    *
@@ -33,35 +32,35 @@ export class EditComponent implements OnInit {
    */
   ngOnInit(): void {
     this.id = this.route.snapshot.params['postId'];
-    this.postService.find(this.id).subscribe((data: Post)=>{
+    this.postService.find(this.id).subscribe((data: Post) => {
       this.post = data;
-    }); 
-       
+    });
+
     this.form = new FormGroup({
       title: new FormControl('', [Validators.required]),
-      body: new FormControl('', Validators.required)
+      body: new FormControl('', Validators.required),
     });
   }
-     
+
   /**
    * Write code on Method
    *
    * @return response()
    */
-  get f(){
+  get f() {
     return this.form.controls;
   }
-     
+
   /**
    * Write code on Method
    *
    * @return response()
    */
-  submit(){
+  submit() {
     console.log(this.form.value);
-    this.postService.update(this.id, this.form.value).subscribe((res:any) => {
-         console.log('Post updated successfully!');
-         this.router.navigateByUrl('post/index');
-    })
+    this.postService.update(this.id, this.form.value).subscribe((res: any) => {
+      console.log('Post updated successfully!');
+      this.router.navigateByUrl('post/index');
+    });
   }
-    
+}
